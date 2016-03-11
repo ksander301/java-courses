998b5b230ebec2e18cf47a9e54ed43fb89315293
Ksander <C:\The Bat!>
2016-03-11 16:27:53 +0300

    create Calculate

---
+++ Calculate.java
@@ -0,0 +1,9 @@
+public class Calculate {
+
+public static void main (String[] args){
+	System.out.println("Starting Calculate....");
+	int a= Integer.valueOf(args[0]);
+	int b= Integer.valueOf(args[1]);
+	System.out.println("Result="+ (a+b));
+}
+}
