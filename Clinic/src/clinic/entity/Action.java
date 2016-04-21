baca47fe8ff7d60a24c8791684ed2bccfca2c9a8
Ksander <C:\The Bat!>
2016-04-21 15:57:37 +0400

    Delete old clinic

--- Clinic/src/clinic/entity/Action.java
+++
@@ -1,7 +0,0 @@
-package clinic.entity;
-
-public interface Action {
-    String operation ();
-
-     void execute(Clinic clinic, Input input, Output output);
-}
--- Clinic/src/clinic/entity/Output.java
+++
@@ -1,9 +0,0 @@
-package clinic.entity;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-
-public interface Output {
-    void Println(String line);
-    void PrintList(String inform,ArrayList listObjects);
-}
